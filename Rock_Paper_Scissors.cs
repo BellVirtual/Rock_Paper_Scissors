bc5f8e5c35be53466d6959507577ce5960af3c3a
BellVirtual <79067920+BellVirtual@users.noreply.github.com>
2021-02-16 14:45:56 -0600

    Add files via upload

---
+++ Rock_Paper_Scissors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Rock_Paper_Scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = " ";
+            Console.Write("Choose your move:(1)Rock (b)Paper (c)Scissors: ");
+            choice = Console.ReadLine();
+            Random rande = new Random();
+            int dice;
+            dice = rande.Next(1, 4);
+
+
+            switch (dice)
+            {
+                case 1: 
+                    Console.WriteLine("Its a tie!");
+                    break;
+
+            }
+
+        }
+    }
+}
